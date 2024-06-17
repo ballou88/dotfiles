dff6df11652bd699fb1600a59fa8f1946eeba59e
Mike Ballou <mike@mikeballou.com>
2024-06-16 17:41:09 -0700

    Adding wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Macchiato"
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"Noto Sans Symbols 2",
+})
+-- and finally, return the configuration to wezterm
+return config
