500c088292dfc31b5271e8a124ea671867a3b7aa
Mike Ballou <mike@mikeballou.com>
2024-09-15 09:05:41 -0700

    Updated my colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-macchiato",
-    },
-  },
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme catppuccin-macchiato]])
+  end,
 }
